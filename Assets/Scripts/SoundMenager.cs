47bbe9534b0a527695d584b4566b3fef970155d7
Tsacube <68530644+Tsacube@users.noreply.github.com>
2020-07-28 23:19:48 +0300

    Make some changes

---
+++ Assets/Scripts/SoundMenager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMenager : MonoBehaviour
+{
+    public AudioSource myFX;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+    public void HoverSound()
+    {
+        myFX.PlayOneShot(hoverFX);
+    }
+    public void ClickSound()
+    {
+        myFX.PlayOneShot(clickFX);
+    }
+}
