d227fd051f38d31fef176d5cccaa47be5aa9a72b
Tsacube <68530644+Tsacube@users.noreply.github.com>
2020-08-02 10:32:43 +0300

    rrr

---
+++ Assets/Scripts/GameSoundManeger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameSoundManeger : MonoBehaviour
+{
+    public GameObject LoseM;
+    public GameObject GameSoundManegerer;
+    public AudioSource myFX;
+    public AudioClip dieFX;
+    void Update()
+    {
+        if(LoseM.active == true)
+        {
+            myFX.PlayOneShot(dieFX);
+            GameSoundManegerer.active = false;      //Не ругайся, пусть живёт
+        }
+    }
+}
